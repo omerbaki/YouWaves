196a41ff42fed616fc09912305b13d760dc89262
omerbaki <bakiomer@yahoo.com>
2014-11-09 16:39:05 +0200

    Add ForeacastAnalysisReport

---
+++ ForecastAnalysisReport/ForecastAnalysisReportCreator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using LevYamWaveAnalyzer;
+
+namespace ForecastAnalysisReport
+{
+    public interface IForecastAnalysisReportCreator
+    {
+        void CreateReport();
+        DateTime LastReportCreatedAt { get; }
+    }
+
+    class ForecastAnalysisReportCreator : IForecastAnalysisReportCreator
+    {
+        private readonly IEnumerable<IWaveAnalyzer> mWaveAnalyzers;
+        private DateTime mLastReportCreatedAt;
+
+        public ForecastAnalysisReportCreator(IEnumerable<IWaveAnalyzer> aWaveAnalyzers)
+        {
+            mWaveAnalyzers = aWaveAnalyzers;
+        }
+
+        public void CreateReport()
+        {
+            //            string reportFileName = GetReportFileName();
+            //
+            //            var analyzersReportsModels = await GetWaveAnalysisReportModel();
+            //            mJsonSerializer.Export(reportFileName, analyzersReportsModels);
+            //
+            //            mLastReportCreatedAt = DateTime.UtcNow;
+        }
+
+
+        public DateTime LastReportCreatedAt
+        {
+            get { return mLastReportCreatedAt; }
+        }
+
+        private string GetReportFileName()
+        {
+            string reportFileName = Path.Combine(DateTime.Now.ToString("yyyyMMdd_HHmm"), ".json");
+            reportFileName = Path.Combine(DateTime.Now.ToString("yyyyMMdd"), reportFileName);
+            return reportFileName;
+        }
+
+        //        private async Task<WaveAnalysisReportModel> GetWaveAnalysisReportModel()
+        //        {
+        //            var waveAnalysisReportModel = new WaveAnalysisReportModel { CreatedAt = DateTime.Now };
+        //
+        //            foreach (var waveAnalyzer in mWaveAnalyzers)
+        //            {
+        //                try
+        //                {
+        //                    waveAnalysisReportModel.AddWaveAnalyzersReport(
+        //                        waveAnalyzer.GetType().Name,
+        //                        await waveAnalyzer.Analyze());
+        //                }
+        //                catch (Exception ex)
+        //                {
+        //                    //mLogger.Error(ex);
+        //                }
+        //            }
+        //
+        //            return waveAnalysisReportModel;
+        //        }
+    }
+}
---
+++ ForecastAnalysisReport/ForecastAnalysisReportScheduler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForecastAnalysisReport
+{
+    public interface IForecastAnalysisReportScheduler
+    {
+        void Start();
+    }
+
+    class ForecastAnalysisReportScheduler : IForecastAnalysisReportScheduler
+    {
+        private const int HOUR_TO_START = 5;
+        private const int HOUR_TO_FINISH = 19;
+        private const int CREATE_REPORT_INTERVAL_MINUTES = 60;
+
+        private readonly IForecastAnalysisReportCreator mForecastAnalysisReportCreator;
+
+        public ForecastAnalysisReportScheduler(IForecastAnalysisReportCreator forecastAnalysisReportCreator)
+        {
+            mForecastAnalysisReportCreator = forecastAnalysisReportCreator;
+        }
+
+        public void Start()
+        {
+            var timer = Observable.Interval(TimeSpan.FromSeconds(5));
+            timer.Subscribe(x => Task.FromResult(RunReportScheduler()));
+        }
+
+        private async Task RunReportScheduler()
+        {
+            try
+            {
+                if (!ShouldCreateReport()) return;
+
+                mForecastAnalysisReportCreator.CreateReport();
+            }
+            catch (Exception ex)
+            {
+                //mLogger.Error(ex);
+            }
+        }
+
+        private bool ShouldCreateReport()
+        {
+            return
+                (DateTime.Now - mForecastAnalysisReportCreator.LastReportCreatedAt).TotalMinutes > CREATE_REPORT_INTERVAL_MINUTES &&
+                IsDaylightTime();
+        }
+
+        private bool IsDaylightTime()
+        {
+            return ((DateTime.Now.Hour > HOUR_TO_START) &&
+                    (DateTime.Now.Hour < HOUR_TO_FINISH));
+        }
+    }
+}
