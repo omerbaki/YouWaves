2d608c2baca1eda655a5b62a3e649e797f695336
omerbaki <bakiomer@yahoo.com>
2014-11-11 18:01:50 +0200

    Create IsramarWaveAnalyzer

---
+++ IsramarWaveAnalyzer/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsramarWaveAnalyzer
+{
+    public class Class1
+    {
+        //http://isramar.ocean.org.il/isramar2009/wave_model/wave_maps/wam/1411110000/coarse/14111112.windir.gif
+    }
+}
