5f5f7adc37796c6112ced5ae5b8f0481fd530464
omerbaki <bakiomer@yahoo.com>
2014-11-16 18:05:10 +0200

    Create basis for EmailSender

---
+++ ForecastNotificationSender/EmailSender.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForecastNotificaitonEntities;
+
+namespace ForecastNotificationSender
+{
+    public class EmailSender
+    {
+        private readonly IWaveForecastNotificationFormatter mWaveForecastNotificationFormatter;
+        public EmailSender(IWaveForecastNotificationFormatter waveForecastNotificationFormatter)
+        {
+            mWaveForecastNotificationFormatter = waveForecastNotificationFormatter;
+        }
+
+        public void Send()
+        {
+            var emailFormat = mWaveForecastNotificationFormatter.GetEmailFormat();
+        }
+    }
+}
---
+++ ForecastNotificationSender/WaveForecastNotificationFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForecastNotificaitonEntities;
+
+namespace ForecastNotificationSender
+{
+    public interface IWaveForecastNotificationFormatter
+    {
+        string GetEmailFormat();
+    }
+
+    class WaveForecastNotificationFormatter : IWaveForecastNotificationFormatter
+    {
+        public string GetEmailFormat()
+        {
+            var notificationsDirectory = Path.Combine("Notifications", DateTime.Now.ToString("yyyyMMdd"));
+            string[] waveForecastNotifications = Directory.GetFiles(notificationsDirectory, typeof(WaveForecastNotification).Name + "*");
+
+            return string.Empty;
+        }
+    }
+}
